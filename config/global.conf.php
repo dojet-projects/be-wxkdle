9b7a5ba06d68caa4e1646ee6ab4a5cca4447fa20
liyan34 <liyan34@lenovo.com>
2016-05-23 17:37:18 +0800

    trace level

--- config/global.conf.php
+++ config/global.conf.php
@@ -1,6 +1,10 @@
 <?php
 $__c = &Config::configRefForKeyPath('global');
 
+$__c['traceLevel'] = array(
+    C_RUNTIME_BWG => Trace::TRACE_ALL,
+);
+
 $__c['log_path'] = array(
     C_RUNTIME_BWG => '/var/log/wxkdle',
     );
